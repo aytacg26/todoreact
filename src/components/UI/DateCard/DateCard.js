44875b726b50a518a745199532348dde92e57115
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-02 21:43:05 +0300

    DateCard component created

---
+++ src/components/UI/DateCard/DateCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './DateCard.module.css';
+
+const DateCard = ({ date }) => {
+  const dateForm = new Date(date);
+  const day = Intl.DateTimeFormat('en-US', { day: '2-digit' }).format(dateForm);
+  const month = Intl.DateTimeFormat('en-US', { month: 'long' }).format(
+    dateForm
+  );
+
+  const year = dateForm.getFullYear();
+
+  return (
+    <div className={classes.DateCard}>
+      <span className={classes.Day}>{day}</span>
+      <span className={classes.Month}>{month}</span>
+      <span className={classes.Year}>{year}</span>
+    </div>
+  );
+};
+
+export default DateCard;
