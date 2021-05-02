89912efddc0fc241ff51e021b18885f90a597517
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-02 16:56:04 +0300

    AddTodo Form Component Created

--- src/components/Layout/AddToDo/AddToDo.js
+++ src/components/Layout/AddToDo/AddToDo.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../UI/Input/Input';
+import Button from '../../UI/Button/Button';
 import classes from './AddTodo.module.css';
+import TodoFilter from './TodoFilter/TodoFilter';
 
 const AddToDo = (props) => {
+  const [showFilter, setShowFilter] = useState(true);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addTodo = () => {};
+  const clearForm = () => {
+    setInputValue('');
+  };
+
+  const toggleFilter = () => {
+    setShowFilter((prevState) => !prevState);
+  };
+
   return (
     <form className={classes.AddTodo}>
-      <Input />
+      <Input
+        type='text'
+        placeholder='Add New ToDo'
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <div className={classes.Actions}>
+        <Button color='blue' type='button' onClick={addTodo}>
+          Add
+        </Button>
+        <Button color='red' type='button' onClick={clearForm}>
+          Clear
+        </Button>
+        <Button color='orange' type='button' onClick={toggleFilter}>
+          {showFilter ? 'Hide Filter' : 'Show Filter'}
+        </Button>
+      </div>
+
+      {showFilter && <TodoFilter />}
     </form>
   );
 };
