067c2b594f6b7a5748314dacd3f1381069635210
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-02 16:57:00 +0300

    ref forward added to Input component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,14 +1,15 @@
+import { forwardRef } from 'react';
 import classes from './Input.module.css';
 import React from 'react';
 
-const Input = (props) => {
+const Input = (props, ref) => {
   const inputClass = props.type === 'text' ? classes.Input : '';
 
   return (
     <div className={inputClass}>
-      <input {...props} />
+      <input {...props} ref={ref} />
     </div>
   );
 };
 
-export default Input;
+export default forwardRef(Input);
