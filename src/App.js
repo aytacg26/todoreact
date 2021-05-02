a9e1a68df08b203a4e81e9b0ee99af99770d7481
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-02 22:38:02 +0300

    Dummy todo and created main components added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,59 @@
+import { useState } from 'react';
 import './App.css';
 import AddToDo from './components/Layout/AddToDo/AddToDo';
-import Input from './components/UI/Input/Input';
+import TodoList from './components/Layout/TodoList/TodoList';
 import Search from './components/UI/Search/Search';
 
-function App() {
+const DUMMY_TODO = [
+  {
+    id: 'todo-12123',
+    title: 'Learn React',
+    description: 'I will try to learn react within 10 months',
+    dateAdded: '2017-06-01',
+    target: 'learn within 10 months',
+    isCompleted: false,
+  },
+  {
+    id: 'todo-12125',
+    title: 'Learn TypeScript',
+    description: 'I will try to learn typescript within 1 month',
+    dateAdded: '2017-07-01',
+    target: 'learn within 1 month',
+    isCompleted: true,
+  },
+  {
+    id: 'todo-12786',
+    title: 'Learn Nodejs',
+    description: 'I will try to learn nodejs within 12 months',
+    dateAdded: '2017-07-06',
+    target: 'learn within 12 months',
+    isCompleted: true,
+  },
+];
+
+const App = () => {
+  const [showFilter, setShowFilter] = useState(false);
+  const [todos, setTodos] = useState(DUMMY_TODO);
+
+  const toggleFilter = () => {
+    setShowFilter((prevState) => !prevState);
+  };
+
+  const addNewToDo = (newTodo) => {
+    setTodos((prevState) => [newTodo, ...prevState]);
+  };
+
   return (
     <div className='container'>
-      <AddToDo />
-      <Search />
-      <Input type='text' name='addTodo' placeholder='Add New Todo' />
+      {showFilter && <Search placeholder='Search ToDo' />}
+      <AddToDo
+        toggleFilter={toggleFilter}
+        showFilter={showFilter}
+        addToList={addNewToDo}
+      />
+      <TodoList todos={todos} />
     </div>
   );
-}
+};
 
 export default App;
