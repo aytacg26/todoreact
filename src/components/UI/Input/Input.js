e9d58561235251260e27fa0433a9030cc713cb02
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-02 14:56:32 +0300

    Input Component Created

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,8 +1,11 @@
+import classes from './Input.module.css';
 import React from 'react';
 
 const Input = (props) => {
+  const inputClass = props.type === 'text' ? classes.Input : '';
+
   return (
-    <div>
+    <div className={inputClass}>
       <input {...props} />
     </div>
   );
