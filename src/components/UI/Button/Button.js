a70533dbff2aab06ca86292a80148cbec8094f07
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-02 14:56:00 +0300

    Button component created

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classes from './Button.module.css';
 
 const Button = (props) => {
+  const [isActive, setActive] = useState(false);
+  const { color } = props;
+
+  const handleClick = () => {
+    setActive(true);
+    const timer = setTimeout(() => {
+      setActive(false);
+      clearTimeout(timer);
+    }, 300);
+  };
+
   return (
-    <div>
-      <button {...props}>{props.children}</button>
+    <div className={classes.btnContainer}>
+      <button
+        {...props}
+        className={`${classes.btn} ${classes[color]}`}
+        onClick={handleClick}
+      >
+        {props.children}
+        <span
+          className={
+            isActive
+              ? `${classes.slideEffect} ${classes.active}`
+              : `${classes.slideEffect}`
+          }
+        ></span>
+      </button>
     </div>
   );
+};
+
+Button.defaultProps = {
+  color: 'blue',
 };
 
 export default Button;
