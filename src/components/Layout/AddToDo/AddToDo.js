c90efbeef5346e098cad229e263202760cad6144
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-02 22:35:23 +0300

    AddToDo component completed

--- src/components/Layout/AddToDo/AddToDo.js
+++ src/components/Layout/AddToDo/AddToDo.js
@@ -1,36 +1,81 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import Input from '../../UI/Input/Input';
 import Button from '../../UI/Button/Button';
 import classes from './AddTodo.module.css';
 import TodoFilter from './TodoFilter/TodoFilter';
+import TextArea from '../../UI/TextArea/TextArea';
 
-const AddToDo = (props) => {
-  const [showFilter, setShowFilter] = useState(true);
-  const [inputValue, setInputValue] = useState('');
+const AddToDo = ({ toggleFilter, showFilter, addToList }) => {
+  const [inputValue, setInputValue] = useState({ title: '', target: '' });
+  const [description, setDescription] = useState('');
+  const textAreaRef = useRef();
 
   const handleInputChange = (e) => {
-    setInputValue(e.target.value);
+    const { name, value } = e.target;
+    setInputValue({ ...inputValue, [name]: value });
   };
 
-  const addTodo = () => {};
-  const clearForm = () => {
-    setInputValue('');
+  const handleTextValue = (text) => {
+    setDescription(text.trim());
   };
 
-  const toggleFilter = () => {
-    setShowFilter((prevState) => !prevState);
+  const clearForm = useCallback(() => {
+    setInputValue({ title: '', target: '' });
+    setDescription('');
+    textAreaRef.current.innerText = '';
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const currentDate = new Date();
+
+    const newTodo = {
+      id: `todo-${currentDate.getTime()}-${currentDate.getFullYear()}`,
+      title: inputValue.title,
+      description,
+      dateAdded: currentDate.toLocaleDateString(),
+      target: inputValue.target,
+      isCompleted: false,
+    };
+
+    addToList(newTodo);
+
+    clearForm();
   };
 
   return (
-    <form className={classes.AddTodo}>
-      <Input
-        type='text'
-        placeholder='Add New ToDo'
-        value={inputValue}
-        onChange={handleInputChange}
+    <form className={classes.AddTodo} onSubmit={handleSubmit}>
+      <div className={classes.InputArea}>
+        <Input
+          type='text'
+          placeholder='TODO Title'
+          value={inputValue.title}
+          onChange={handleInputChange}
+          name='title'
+        />
+
+        <Input
+          type='text'
+          placeholder='Target'
+          value={inputValue.target}
+          onChange={handleInputChange}
+          name='target'
+        />
+      </div>
+      <TextArea
+        maxLength={180}
+        counterText='remaining'
+        value={description}
+        showCounter
+        placeHolder='Description'
+        name='description'
+        onChange={handleTextValue}
+        ref={textAreaRef}
       />
+
       <div className={classes.Actions}>
-        <Button color='blue' type='button' onClick={addTodo}>
+        <Button color='blue' type='submit'>
           Add
         </Button>
         <Button color='red' type='button' onClick={clearForm}>
