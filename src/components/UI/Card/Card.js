fb8599f6054cd5559c777f5e43bd7680ab307ddc
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-04 00:24:42 +0300

    Card component updated according to new styles

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import classes from './Card.module.css';
 
-const Card = (props) => {
-  return <div className={classes.Card}>{props.children}</div>;
+const Card = ({ children, row, column, center }) => {
+  const cardClass = row
+    ? `${classes.Card} ${classes.Row}`
+    : column
+    ? `${classes.Card} ${classes.Column}`
+    : classes.Card;
+
+  return (
+    <div className={`${cardClass} ${center ? classes.Center : ''}`}>
+      {children}
+    </div>
+  );
 };
 
 export default Card;
