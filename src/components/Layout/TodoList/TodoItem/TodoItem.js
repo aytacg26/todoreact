a811448a4a13d7e06f2bab24d7efe1b4d58353ad
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-04 00:23:41 +0300

    TodoItem comp. completed with all functionality

--- src/components/Layout/TodoList/TodoItem/TodoItem.js
+++ src/components/Layout/TodoList/TodoItem/TodoItem.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../../../UI/Button/Button';
 import Card from '../../../UI/Card/Card';
 import DateCard from '../../../UI/DateCard/DateCard';
 import classes from './TodoItem.module.css';
 
-const TodoItem = ({ title, completed, date }) => {
+const TodoItem = ({
+  title,
+  completed,
+  date,
+  target,
+  description,
+  onComplete,
+  onDelete,
+}) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleDetails = () => {
+    setShowDetails((prevState) => !prevState);
+  };
+
   return (
     <li>
-      <Card>
+      <Card column center>
         <div className={classes.TodoBand}>
           <div className={classes.DateSection}>
             <DateCard date={date} />
@@ -19,30 +33,44 @@ const TodoItem = ({ title, completed, date }) => {
             <div className={classes.completedMark}></div>
           )}
           <div className={classes.Actions}>
-            <Button color='green'>Completed</Button>
-            <Button color='blue'>Show Details</Button>
-            <Button color='red'>Delete</Button>
+            <Button
+              color={!completed ? 'green' : 'orange'}
+              onClick={onComplete}
+            >
+              {completed ? 'Incompleted' : 'Completed'}
+            </Button>
+            <Button color='blue' onClick={handleDetails}>
+              {!showDetails ? 'Show' : 'Hide'} Details
+            </Button>
+            <Button color='red' onClick={onDelete}>
+              Delete
+            </Button>
           </div>
         </div>
-        <div className={classes.Details}>
-          <div>Target</div>
-          <div>Description</div>
-          <div>Completed Mark</div>
-        </div>
+
+        {showDetails && (
+          <div className={classes.Details}>
+            <div className={classes.Content}>
+              <div className={classes.Target}>
+                <label className={classes.Label}>Target : </label>
+                <span>{target}</span>
+              </div>
+              <span className={classes.Description}>{description}</span>
+              <div className={classes.Status}>
+                {' '}
+                <label className={classes.Label}>Status :</label>
+                {completed ? (
+                  <div className={classes.completedMark}>&#10003;</div>
+                ) : (
+                  <div className={classes.incompleteMark}>&#10060;</div>
+                )}
+              </div>
+            </div>
+          </div>
+        )}
       </Card>
     </li>
   );
 };
 
 export default TodoItem;
-
-/**
- *   {
-    id: 'todo-12125',
-    title: 'Learn TypeScript',
-    description: 'I will try to learn typescript within 1 month',
-    dateAdded: '2017-07-01',
-    target: 'learn within 1 month',
-    isCompleted: true,
-  },
- */
