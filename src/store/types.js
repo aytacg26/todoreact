7634feb1f41bcb31e06522a5fbec92bf091aa4ec
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-04 17:58:07 +0300

    modal types added to redux type constants

--- src/store/types.js
+++ src/store/types.js
@@ -3,3 +3,5 @@ export const DELETE_TODO = 'DELETE_TODO';
 export const SET_STATUS = 'SET_STATUS';
 export const FILTER_TODO = 'FILTER_TODO';
 export const SEARCH_TODO = 'SEARCH_TODO';
+export const SHOW_MODAL = 'SHOW_MODAL';
+export const HIDE_MODAL = 'HIDE_MODAL';
