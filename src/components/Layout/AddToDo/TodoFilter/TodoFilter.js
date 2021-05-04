6692751d4da83c63f2aecf27fec62f296d022e53
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-04 12:30:39 +0300

    filterTodo redux Action added to TodoFilter comp.

--- src/components/Layout/AddToDo/TodoFilter/TodoFilter.js
+++ src/components/Layout/AddToDo/TodoFilter/TodoFilter.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Radio from '../../../UI/Radio/Radio';
 import classes from './TodoFilter.module.css';
+import { filterTodo } from '../../../../store/Actions/todoActions/todoActions';
+import { useDispatch } from 'react-redux';
 
-const TodoFilter = ({ onFilter }) => {
+const TodoFilter = () => {
+  const dispatch = useDispatch();
+
+  const handleFilter = (e) => {
+    dispatch(filterTodo(e.target.value));
+  };
+
   return (
     <div className={classes.FilterToDo}>
       <Radio
         id='filter-check-all'
         value='all'
-        onChange={onFilter}
+        onChange={handleFilter}
         color='blue'
         label='All'
         name='filter-todo'
@@ -16,7 +24,7 @@ const TodoFilter = ({ onFilter }) => {
       <Radio
         id='filter-check-completed'
         value='completed'
-        onChange={onFilter}
+        onChange={handleFilter}
         color='blue'
         label='Completed'
         name='filter-todo'
@@ -24,7 +32,7 @@ const TodoFilter = ({ onFilter }) => {
       <Radio
         id='filter-check-incomplete'
         value='incomplete'
-        onChange={onFilter}
+        onChange={handleFilter}
         color='blue'
         label='Incomplete'
         name='filter-todo'
