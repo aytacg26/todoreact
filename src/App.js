6ce7385e099a6cd088960c778bf098f539cdb1c7
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-04 15:14:30 +0300

    todos added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import { Provider } from 'react-redux';
 import appStore from './store/store';
 import Todo from './components/Layout/Todo/Todo';
 
+/**
+ *
+ * @TODO :
+ *        -- Add Modal, if user does not enter anything to required fields, user should be warned with the help of modal
+ *         - Add warning css to Input, for required fields, Input border and background should warn user
+ *         - Add lazyloading to AddToDo for filter window, and add lazyloading for details in TodoItem
+ *
+ */
+
 const App = () => {
   return (
     <Provider store={appStore}>
