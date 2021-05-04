6995afd40356e3c8e460108a135f1c8a4daa60ae
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-04 17:56:00 +0300

    Input component updated according to Modal

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,18 +4,26 @@ import classes from './Input.module.css';
 import React from 'react';
 
 const Input = (props) => {
-  // const [isEmpty, setIsEmpty] = useState(false);
   const inputEl = useRef();
   const { isEmpty } = useValidate(inputEl, props.value);
 
+  //isrequired is not an attribute of input element and required attribute of input element prevents the work of alert
+  //for this reason, isrequired used and to prevent error message of React, for DOM input element, new props object created
+  //(a copy of props) and assigned to the DOM input element (inElProps => input element props, copy of props without isrequired)
+
   const inputClass =
     props.type === 'text'
-      ? `${classes.Input} ${isEmpty && props.required ? classes.Required : ''}`
+      ? `${classes.Input} ${
+          isEmpty && props.isrequired ? classes.Required : ''
+        }`
       : '';
+
+  const inElProps = { ...props };
+  delete inElProps['isrequired'];
 
   return (
     <div className={inputClass}>
-      <input {...props} ref={inputEl} />
+      <input {...inElProps} ref={inputEl} />
     </div>
   );
 };
