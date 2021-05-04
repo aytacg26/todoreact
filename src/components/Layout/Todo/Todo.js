bdb9b4db6632d7b71f38d3f03d4e1ebe749f9ca9
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-04 18:37:52 +0300

    Lazy loading added for Search Component

--- src/components/Layout/Todo/Todo.js
+++ src/components/Layout/Todo/Todo.js
@@ -1,9 +1,11 @@
-import React, { Fragment, useState } from 'react';
-import Search from '../../UI/Search/Search';
+import React, { Fragment, lazy, Suspense, useState, useCallback } from 'react';
+
 import AddToDo from '../AddToDo/AddToDo';
 import TodoList from '../TodoList/TodoList';
 import { useDispatch } from 'react-redux';
 import { searchTodo } from '../../../store/Actions/todoActions/todoActions';
+import Loader from '../../UI/Loader/Loader';
+const Search = lazy(() => import('../../UI/Search/Search'));
 
 const Todo = () => {
   const [showFilter, setShowFilter] = useState(false);
@@ -11,9 +13,9 @@ const Todo = () => {
 
   const dispatch = useDispatch();
 
-  const toggleFilter = () => {
+  const toggleFilter = useCallback(() => {
     setShowFilter((prevState) => !prevState);
-  };
+  }, []);
 
   const handleSearchChange = (e) => {
     const entered = e.target.value;
@@ -25,11 +27,13 @@ const Todo = () => {
   return (
     <Fragment>
       {showFilter && (
-        <Search
-          placeholder='Search ToDo'
-          onChange={handleSearchChange}
-          value={search}
-        />
+        <Suspense fallback={<Loader />}>
+          <Search
+            placeholder='Search ToDo'
+            onChange={handleSearchChange}
+            value={search}
+          />
+        </Suspense>
       )}
       <AddToDo toggleFilter={toggleFilter} showFilter={showFilter} />
       <TodoList />
