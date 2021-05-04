854bd99e1894e28a9ae5371f41ba76920e540abe
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-04 17:59:52 +0300

    modalReducer combined with other reducers.

--- src/store/Reducers/index.js
+++ src/store/Reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import modalReducer from './ModalReducer/modalReducer';
 import todoReducer from './TodoReducer/todoReducer';
 
 //This is our rootReducer and we will combine our reducers with the help of combineReducers func of redux
 
 export default combineReducers({
   todo: todoReducer,
+  modal: modalReducer,
 });
