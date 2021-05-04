fee9bc7a29697d91d910111b257f4be091705fab
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-05-04 12:28:29 +0300

    Redux store and types created

---
+++ src/store/types.js
@@ -0,0 +1,5 @@
+export const ADD_TODO = 'ADD_TODO';
+export const DELETE_TODO = 'DELETE_TODO';
+export const SET_STATUS = 'SET_STATUS';
+export const FILTER_TODO = 'FILTER_TODO';
+export const SEARCH_TODO = 'SEARCH_TODO';
